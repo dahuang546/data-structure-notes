dcd54e32db04a90c3b9850bf5c71016002b6201b
dahuang546 <dahuang546@users.noreply.github.com>
2026-04-23 14:24:26 +0800

    feat(串): 添加 copyStr 函数及注释

--- 串/char.c
+++ 串/char.c
@@ -12,6 +12,7 @@ void clearStr(Str *s){
     s->length=0;
     s->ch[0]='\0';
 }
+//再写拼接函数
 void concatantionStr(Str *s,Str *t){
     if(s->length+t->length>=100){
         printf("error,thats more");
@@ -30,8 +31,14 @@ void concatantionStr(Str *s,Str *t){
         }
 
 }
-
+//初始化函数**实际上就是全部化空哈哈
 void initStr(Str *s){
     s->ch[0]='\0';
     s->length=0;
+}
+//接下来是复制函数，实际上就是把一个串先清空再拼接，没啥好说的
+void copyStr(Str *s,Str *t){
+    clearStr(s);
+    concatantionStr(s,t);
+    printf("拼接完毕，你可以查看一下: ",&s);
 }
